8b3dd49797a69ea6840035159e345aa2a6added4
Ángel <angel.angel802@gmail.com>
2019-12-26 15:31:22 +0100

    Fixing Puma configuration file

--- config/puma.rb
+++ config/puma.rb
@@ -12,5 +12,7 @@ on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/
   # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+  # ActiveRecord::Base.establish_connection
 end
+
+config/puma.rb:15:in `block in _load_from': uninitialized constant #<Class:#<Puma::DSL:0x00005605e61a8eb0>>::ActiveRecord (NameError)
