23dc751402f7ddf326ad35cd7bf4d32a42149702
Ángel <angel.angel802@gmail.com>
2019-12-27 17:59:17 +0100

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User
   field :name, type: String
   field :email, type: String
   field :password_digest, type: String
+  field :remember_digest, type: String
+
+  attr_accessor :remember_token
 
   index({ email: 1 }, { unique: true })
 
@@ -24,5 +27,26 @@ class User
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
---
+++ test/helpers/sessions_helper_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class SessionsHelperTest < ActionView::TestCase
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+    @user.save
+    remember(@user)
+  end
+
+  test "current_user returns right user when session is nil" do
+    assert_equal @user, current_user
+    assert is_logged_in?
+  end
+
+  test "current_user returns nil when remember digest is wrong" do
+    @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    assert_nil current_user
+  end
+
+  def teardown
+    @user.destroy
+  end
+end
