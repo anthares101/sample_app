5ee1c139f030835ff26e526c6f9cb76a19417744
Ángel <angel.angel802@gmail.com>
2019-12-31 00:14:05 +0100

    Added update user functionality, added friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,8 +12,13 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-   # Returns the current logged-in user (if any).
-   def current_user
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
+  # Returns the current logged-in user (if any).
+  def current_user
     if (user_id = session[:user_id]) && !User.where(id: user_id).nil?
       @current_user ||= User.find(id: user_id)
     elsif (user_id = cookies.signed[:user_id]) && !User.where(id: user_id).nil?
@@ -41,5 +46,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
